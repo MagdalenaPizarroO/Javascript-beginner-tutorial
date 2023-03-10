72bc0c77b40fd1479530748a8d72d516085490f7
MagdalenaPizarroO <mspizarro.ortega@gmail.com>
2023-03-10 23:47:46 +0000

    trabajando en la 23

--- exercises/23-Bottles_of_milk/app.js
+++ exercises/23-Bottles_of_milk/app.js
@@ -1,1 +1,17 @@
 // Your code here:
+// function sing() {
+//     let str = "";
+//     for(let i = 0; i < 100; i++){
+//         if(i === 4) str += 'whisper words of wisdom, ';
+//         else if (i === 10) str += 'there will be an answer, let it be';
+//         else str += 'let it be, ';
+//     }
+//     return str;
+// }
+
+function sing() {
+    let str = "";
+    for (let i = 99; i >=0; i--) {
+        
+    }
+}
